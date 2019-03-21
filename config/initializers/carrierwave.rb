13e6b7bbe9bc94331a399026011526e6274e184d
Eric Lubow <eric@lubow.org>
2019-03-21 16:27:58 +0100

    removing IAM from carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -17,7 +17,6 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     :provider               => 'AWS',
-    :use_iam_profile        => true,
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'] || '',
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'] || '',
     :region                 => ENV['S3_REGION_NAME'] || 'us-east-1'
