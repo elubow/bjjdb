fac418b7ce41968b6dd5cfd603f2b332ca015148
Eric Lubow <eric@lubow.org>
2019-03-21 09:39:27 +0100

    env issues

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -17,9 +17,9 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-    :region                 => ENV['S3_REGION_NAME']
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'] || '',
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'] || '',
+    :region                 => ENV['S3_REGION_NAME'] || 'us-east-1'
   }
-  config.fog_directory  = ENV['S3_BUCKET_NAME']
+  config.fog_directory  = ENV['S3_BUCKET_NAME'] || 'bjjdb-test'
 end
