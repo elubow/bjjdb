3a3579d613c57fb68ba97fd8de5e48b1fe05be38
Eric Lubow <eric@lubow.org>
2019-03-21 10:46:18 +0100

    carrierwave changes

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -17,9 +17,11 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     :provider               => 'AWS',
+    :use_iam_profile        => true,
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'] || '',
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'] || '',
     :region                 => ENV['S3_REGION_NAME'] || 'us-east-1'
   }
   config.fog_directory  = ENV['S3_BUCKET_NAME'] || 'bjjdb-test'
+  config.storage = :fog
 end
