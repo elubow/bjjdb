2f471ffc2ea73ee08abc38793623dd87ed733e00
WebDevStar730 <webdevstarjus730@gmail.com>
2019-03-25 13:53:45 -0400

    Adding facebook login feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,25 @@ class User < ApplicationRecord
   has_many :login_activities, as: :user
 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "only allows valid emails" }
+  devise :omniauthable, omniauth_providers: [:facebook]
 
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+  
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+      user.image = auth.info.image # assuming the user model has an image
+    end
+  end
+  
   def set_default_role
     self.role ||= :user
   end
@@ -16,7 +34,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook]
 
   cattr_accessor :current_user
   # instead of deleting, indicate the user requested a delete & timestamp it
