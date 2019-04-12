1ba9da1bde34bc9d3665d2f7af3901769791ca05
Runtime <runtime@Runtimes-MacBook-Pro.local>
2019-04-12 14:53:49 +0100

    Added embedded videos

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,2 +1,16 @@
 module LinksHelper
+    def link_url_helper(height = 420, link)
+        link_url = link.url.split('/')
+        if link_url[2].include? "youtube"
+            link_url[2] += '/embed'
+            link_url[3] = link_url[3].split('=')[1]
+        else
+            link_url[-1] += '/embed/captioned'
+            height = 780
+        end
+        #780
+        iframe = "<iframe width='100%' height='" + height.to_s + "' src='" + link_url.join('/') + "'></iframe>"
+        puts iframe
+        return iframe.html_safe
+    end
 end
