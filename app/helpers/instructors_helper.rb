3c92972afa6c9d992c83c7e143fabd0ced74db79
Runtime <runtime@Runtimes-MacBook-Pro.local>
2019-04-18 14:15:40 +0100

    Lacked a condition that would allow a nil to pass

--- app/helpers/instructors_helper.rb
+++ app/helpers/instructors_helper.rb
@@ -1,15 +1,15 @@
 module InstructorsHelper
     def social_media_helper(instructor)
-        insta_link = "https://instagram.com/" + instructor.instagram[1..-1]
-        twitter_link = "https://twitter.com/" + instructor.twitter[1..-1]
 
-        youtube_link = "https://youtube.com/user/" + instructor.youtube
+        if !instructor.instagram.empty? && !instructor.instagram[1..-1].empty?
+            instagram = "<a href='" + "https://instagram.com/" + instructor.instagram[1..-1] + "'>" + "https://instagram.com/" + instructor.instagram[1..-1] + "</a></br>" 
+        end
+        if !instructor.twitter.empty? && !instructor.twitter[1..-1].empty?
+            twitter = "<a href='" + "https://twitter.com/" + instructor.twitter[1..-1] + "'>" + "https://twitter.com/" + instructor.twitter[1..-1] + "</a></br>"
+        end
 
-        instagram = "<a href='" + insta_link + "'>" + insta_link + "</a></br>" if !instructor.instagram[1..-1].empty? && !instructor.instagram.empty?
-        twitter = "<a href='" + twitter_link + "'>" + twitter_link + "</a></br>" if !instructor.twitter[1..-1].empty? && !instructor.twitter.empty?
         facebook = "<a href='" + instructor.facebook + "'>" + instructor.facebook + "</a></br>" if !instructor.facebook.empty?
-        youtube = "<a href='" + youtube_link + "'>" + youtube_link + "</a>" if !instructor.youtube.empty?
-
+        youtube = "<a href='" + "https://youtube.com/user/" + instructor.youtube + "'>" + "https://youtube.com/user/" + instructor.youtube + "</a>" if !instructor.youtube.empty?
         final_string = [instagram, twitter, facebook, youtube].reject{ |link| link == nil }
 
         return final_string.join('').html_safe
