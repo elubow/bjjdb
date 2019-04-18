aeadee40632cdb823eab7a82f07582f5ffbc887f
Runtime <runtime@Runtimes-MBP.lan>
2019-04-18 16:03:37 +0100

    Problem might be nil values from DB

--- app/helpers/instructors_helper.rb
+++ app/helpers/instructors_helper.rb
@@ -1,15 +1,16 @@
 module InstructorsHelper
     def social_media_helper(instructor)
 
-        if !instructor.instagram.empty? && !instructor.instagram[1..-1].empty?
+        
+        if !instructor.instagram.nil? && !instructor.instagram.empty? && !instructor.instagram[1..-1].empty?
             instagram = "<a href='" + "https://instagram.com/" + instructor.instagram[1..-1] + "'>" + "https://instagram.com/" + instructor.instagram[1..-1] + "</a></br>" 
         end
-        if !instructor.twitter.empty? && !instructor.twitter[1..-1].empty?
+        if !instructor.twitter.nil? && !instructor.twitter.empty? && !instructor.twitter[1..-1].empty?
             twitter = "<a href='" + "https://twitter.com/" + instructor.twitter[1..-1] + "'>" + "https://twitter.com/" + instructor.twitter[1..-1] + "</a></br>"
         end
 
-        facebook = "<a href='" + instructor.facebook + "'>" + instructor.facebook + "</a></br>" if !instructor.facebook.empty?
-        youtube = "<a href='" + "https://youtube.com/user/" + instructor.youtube + "'>" + "https://youtube.com/user/" + instructor.youtube + "</a>" if !instructor.youtube.empty?
+        facebook = "<a href='" + instructor.facebook + "'>" + instructor.facebook + "</a></br>" if !instructor.facebook.nil? && !instructor.facebook.empty?
+        youtube = "<a href='" + "https://youtube.com/user/" + instructor.youtube + "'>" + "https://youtube.com/user/" + instructor.youtube + "</a>" if !instructor.youtube.nil? && !instructor.youtube.empty?
         final_string = [instagram, twitter, facebook, youtube].reject{ |link| link == nil }
 
         return final_string.join('').html_safe
