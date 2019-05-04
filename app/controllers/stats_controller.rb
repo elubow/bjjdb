c6518abfe8082df78174107aa7434fdb28a67f05
Eric Lubow <eric@lubow.org>
2019-05-04 22:28:37 +0200

    stats: added back thumbnail model counter

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,7 +2,7 @@ class StatsController < ApplicationController
 
   def site
     @stats = {}
-    [User, Favorite, Rating, Instructor, Tag, Impression, Comment, PrivateNote].each do |model|
+    [User, Favorite, Rating, Instructor, Tag, Impression, Comment, Thumbnail, PrivateNote].each do |model|
       @stats[model.to_s] = model.count
     end
     @links = Stat.collect_link_stats
