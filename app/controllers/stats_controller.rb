b55fdb11142c1bdac0a86858fe8d352e8de62053
Eric Lubow <eric@lubow.org>
2019-05-04 22:22:55 +0200

    stats: adding more usage stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,11 +2,12 @@ class StatsController < ApplicationController
 
   def site
     @stats = {}
-    [User, Instructor, Tag, Impression, Comment, Thumbnail, PrivateNote].each do |model|
+    [User, Favorite, Rating, Instructor, Tag, Impression, Comment, PrivateNote].each do |model|
       @stats[model.to_s] = model.count
     end
     @links = Stat.collect_link_stats
     @links["Average Tags Per Video"] = Stat.average_tags_per_video
-
+    @links["Videos with Ratings"] = Rating.pluck(:link_id).uniq.count
+    @links["Videos with Favorites"] = Favorite.pluck(:link_id).uniq.count
   end
 end
