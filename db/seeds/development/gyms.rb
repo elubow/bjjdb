20da0f09da71fa06eaca6dd6bb4fea70a8c59a21
Eric Lubow <eric@lubow.org>
2020-01-19 16:30:43 +0100

    reviews: adding seed data for gyms

--- db/seeds/development/gyms.rb
+++ db/seeds/development/gyms.rb
@@ -11,5 +11,22 @@ Gym.create!(
   city: 'Colorado Springs',
   state: 'Colorado',
   postal_code: '80910',
-  country: 'US'
+  country: 'United States',
+  aasm_state: 'published'
+)
+
+Gym.create!(
+  id: 2,
+  name: 'Marcelo Garcia Academy',
+  website: 'https://www.mginaction.com/',
+  affiliation: 'Marcelo Garcia',
+  address_full: '250 West 26th Street, New York, NY, USA',
+  address_1: '250 West 26th Street',
+  city: 'New York',
+  state: 'NY',
+  postal_code: '10001',
+  country: 'United States',
+  latitude: 0.407466825e2,
+  longitude: -0.739962173e2,
+  aasm_state: 'published'
 )
